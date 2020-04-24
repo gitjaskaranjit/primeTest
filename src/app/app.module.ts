d6ba69453864e008d5ec15afd866611c692f0513
gitjaskaranjit <jaskaran.sd@live.ca>
2020-04-24 16:25:44 -0600

    tested dropdown component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +13,9 @@ import { DropdownModule } from "primeng/dropdown";
   ],
   imports: [
     BrowserModule,
-    DropdownModule
+    DropdownModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
