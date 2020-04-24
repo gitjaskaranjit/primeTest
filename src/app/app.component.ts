a16261b518aded09bd1ac0c2506845f384b33fea
gitjaskaranjit <jaskaran.sd@live.ca>
2020-04-24 16:05:43 -0600

    before update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primeTest';
+  cities: City[];
+  constructor(){
+ this.cities=[
+   {
+     Id:1,
+     Name: "Toronto"
+   },
+   {
+     Id:2,
+     Name:"Brampton"
+   },
+   {
+     Id:3,
+     Name:"Missisauga"
+   }
+ ]
+  }
+}
+interface City{
+  Id: number
+  Name: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { DropdownModule } from "primeng/dropdown";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
